0e921237aa03654442369734b0cd6f287fde53f0
kalebrobi <kalebrobi10@gmail.com>
2023-03-07 12:09:32 -0700

    typo

--- app/models/like.py
+++ app/models/like.py
@@ -4,8 +4,8 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 likes = db.Table(
   'likes',
   db.Model.metadata,
-  db.Colum('users', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
-  db.Colum('transactions', db.Integer, db.ForeignKey(add_prefix_for_prod('transactions.id')), primary_key=True),
+  db.Column('users', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
+  db.Column('transactions', db.Integer, db.ForeignKey(add_prefix_for_prod('transactions.id')), primary_key=True),
 )
 
 if environment == 'production':
